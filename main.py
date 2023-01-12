89a0e614a5c451b0d8b87b628c3d0d31089769df
Abdurahman Abukar <abzabukar99@gmail.com>
2023-01-12 01:45:37 +0000

    Update port number in main.py aligned with Dockerfile and kubernetes manifest

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000, host='0.0.0.0')
+    app.run(debug=True, port=5000, host='0.0.0.0')
